582cb6ea5f7e2d68c187a9a6c84e50b583ad81d0
Colin McGlone <me@colinmcglone.ca>
2018-09-27 23:19:35 -0400

    delete all thumbs and redo to fix badly generated ones

--- photos.py
+++ photos.py
@@ -40,11 +40,11 @@ def update_photos():
 	if not albums:
 		return render_template('update.html', body = 'No Albums')
 
-	shutil.rmtree('./static/media/Bon_Echo/thumbs')
+#	shutil.rmtree('./static/media/Bon_Echo/thumbs')
 
 	for album in albums:
-#		if exists(album['album_directory'] + '/thumbs/'):
-#			shutil.rmtree(album['album_directory'] + '/thumbs/')
+		if exists(album['album_directory'] + '/thumbs/'):
+			shutil.rmtree(album['album_directory'] + '/thumbs/')
 		if not exists(album['album_directory'] + '/thumbs/'):
 			makedirs(album['album_directory'] + '/thumbs/')
 		for photo in album['photos']:
